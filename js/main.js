467a67a53f34440bdf3524763d61ff9d2c4f9b31
Nathan Motoki <nathan_motoki@hotmail.com>
2021-09-08 13:52:18 -0300

    Criando telas de login - Professor e Aluno

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var botao1 = document.getElementsByClassName("one");
+var botao2 = document.getElementsByClassName("two");
 
 function clicar(botao1) {
     window.location.href = 'loginProfessor.html'
+}
+
+function clicar1(botao2) {
+    window.location.href = 'loginAluno.html'
 }
